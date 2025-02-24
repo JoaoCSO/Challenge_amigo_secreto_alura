82c76f754d3bb33412cef36e43f12a1af3cd340b
JoaoCSO <joaocarlossilvaoliveira03@gmail.com>
2025-02-24 10:08:44 -0300

    Adicionada a validação de entrada para garantir que o campo não esteja vazio antes de adicionar nomes ao array de amigos.

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//Array feito para armazenar os nomes dos amigos
+let amigos = [];
+
+function validarEntrada(nome) {
+    if (nome.trim() === '') {
+        alert('Por favor, insira um nome.');
+        return false;
+    } else {
+        return true;
+    }
+}
+
