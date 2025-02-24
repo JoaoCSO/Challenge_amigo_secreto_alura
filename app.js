abc80d2fe1c7ebd9a08e022a9381eb94e06af286
JoaoCSO <joaocarlossilvaoliveira03@gmail.com>
2025-02-24 11:09:11 -0300

    Implementada funções para atualizar a lista de amigos no HTML, limpar o campo de entrada após adicionar um nome e a lógica inicial para o sorteio.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
 //Array feito para armazenar os nomes dos amigos
 let amigos = [];
 
@@ -10,4 +11,34 @@ function validarEntrada(nome) {
         return true;
     }
 }
+
+function adicionarAmigo() {
+    const input = document.getElementById('amigo');
+    const nome = input.value;
+
+    if (validarEntrada(nome)) {
+        amigos.push(nome);
+        atualizarListaAmigos();
+        input.value = ''; //Limpa o campo de entrada após adicionar o nome
+    }
+}
+
+function atualizarListaAmigos() {
+    const listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = ''; //Limpa a lista antes de atualizá-la
+
+    amigos.forEach(amigo => {
+        const li = document.createElement('li');
+        li.textContent = amigo;
+        listaAmigos.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length < 2) {
+        alert('Erro: É necessário pelo menos dois amigos para realizar o sorteio')
+        return;
+    }
+}
+
 
